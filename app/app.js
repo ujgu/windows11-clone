6e9daa872afef8eda322f99bc91956a6bd55714c
ujgu <utkuipek07@hotmail.com>
2021-07-05 17:18:14 +0300

    something something

--- app/app.js
+++ app/app.js
@@ -37,7 +37,7 @@ let startmenu = document.getElementsByClassName("startmenu")[0];
 taskbar.addEventListener("click", () => {
   console.log("clicked");
   if (startmenu.style.bottom == "50px") {
-    startmenu.style.bottom = "-660px";
+    startmenu.style.bottom = "-680px";
   } else {
     startmenu.style.bottom = "50px";
   }
